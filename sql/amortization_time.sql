3934c66c357b22f533db0b924b0fccd9dc2e588b
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-01-13 15:53:32 +0100

    chore: reestructuring comments and where the code goes between the comments + cleaned & converted value type to correct value type

--- sql/amortization_time.sql
+++ sql/amortization_time.sql
@@ -5,15 +5,26 @@
 WITH bikeumeter_activities AS (
     SELECT * FROM read_csv_auto('data/bikeumeter_activities.csv') AS activities
     CROSS JOIN read_csv_auto('data/bikeumeter_bike_costs.csv') AS costs
-)
+),
 -- 3) get the total which is the amount spent on the bike
     -- a) strip the public_transport_fare of the euro symbol
+cleaned_fare AS (
+    SELECT *, 
+            REPLACE(public_transport_fare, '€', '') AS cleaned_fare
+    FROM bikeumeter_activities
+),
     -- b) convert it to a numeric value
+numeric_fare AS (
+    SELECT *,
+            CAST(cleaned_fare AS DECIMAL(10, 2)) AS numeric_fare
+    FROM cleaned_fare
+)
     -- c) convert total to negative value
     -- c) do the cumulative sum
 SELECT date, ride_duration_minutes, 
-        public_transport_fare, total, (total * -1) AS bike_cost
-FROM bikeumeter_activities
+        public_transport_fare, total, (total * -1) AS bike_cost,
+        numeric_fare
+FROM numeric_fare
 LIMIT 10;
 -- 4) check to see when it crosses 0 using running total against total spent on bike
 
