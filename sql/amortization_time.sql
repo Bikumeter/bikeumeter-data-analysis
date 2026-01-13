f40601128289b65e221f91c0504ca7ef05e3668e
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-01-13 16:20:28 +0100

    fix: using a scalar subquery to get only one row from the other table

--- sql/amortization_time.sql
+++ sql/amortization_time.sql
@@ -1,10 +1,15 @@
 -- 1) create temporary table with the data in the csv files to be able to reuse without having to re import using read_csv_auto
--- 2) join both bikeumeter_activities + bikeumeter_bike_costs
-    -- what type of join? => CROSS JOIN => tables don't have columns in common, 
-    -- so I need all the info from one table, and just one col from the second.
+-- 2) I need only one constant from one table and all the data from the other
+
 WITH bikeumeter_activities AS (
-    SELECT * FROM read_csv_auto('data/bikeumeter_activities.csv') AS activities
-    CROSS JOIN read_csv_auto('data/bikeumeter_bike_costs.csv') AS costs
+    SELECT *, 
+        -- a) a scalar subquery is the best for this, no joining
+        (
+            SELECT total 
+            FROM read_csv_auto('data/bikeumeter_bike_costs.csv')
+            LIMIT 1
+        )  AS bike_cost
+    FROM read_csv_auto('data/bikeumeter_activities.csv') AS activities
 ),
 -- 3) get the total which is the amount spent on the bike
     -- a) strip the public_transport_fare of the euro symbol
@@ -22,7 +27,7 @@ numeric_fare AS (
     -- c) convert total to negative value
     -- d) do the cumulative sum
 SELECT date, ride_duration_minutes, 
-        public_transport_fare, total, (total * -1) AS bike_cost,
+        public_transport_fare, bike_cost, (bike_cost * -1) AS bike_cost,
         numeric_fare,
         SUM(numeric_fare) OVER( ORDER BY date)
 FROM numeric_fare
