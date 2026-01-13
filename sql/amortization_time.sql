fe94a7934a0378b01ca34fbb544dc9b2942f9fb5
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-01-13 12:00:38 +0100

    chore: added tasks as comments to structure my thinking process

--- sql/amortization_time.sql
+++ sql/amortization_time.sql
@@ -1,3 +1,8 @@
+-- 1) join both bikeumeter_activities + bikeumeter_bike_costs
+-- 2) get the total which is the amount I spent on the bike
+-- 3) do a running total = cumulative sum
+-- 4) check to see when it crosses 0 using running total against total spent on bike
+
 SELECT *
 FROM read_csv_auto('data/bikeumeter_activities.csv')
 LIMIT 10;
