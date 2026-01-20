19c7d6090be338f85a8a8a3a7b28bfb1e178d7fb
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-01-20 17:25:00 +0100

    chore: rearranging and deleting comments

--- sql/monthly_bike_savings.sql
+++ sql/monthly_bike_savings.sql
@@ -1,50 +1,49 @@
-WITH bikeumeter_activities AS (
-    SELECT date, start_city, end_city, public_transport_fare, ride_duration_minutes
-    FROM read_csv_auto('data/bikeumeter_commute_activities.csv') AS activities
-),
 
--- 1) Date manipulation
-    -- a) Keep only date portion of timestamp
-clean_date AS (
-    SELECT *,
-            CAST(date AS DATE) AS date_only
-    FROM bikeumeter_activities
-),
 
--- b) Turn date into month -- monthname(date) function
--- c) Group activities by date
-monthly_activities AS (
-    SELECT date_part('month', date_only) AS month_number, MONTHNAME(date_only) AS month, public_transport_fare, ride_duration_minutes,
-    FROM clean_date
-    WHERE date_part('year', date_only) = 2025
-    ORDER BY date_only -- always order by date not by month name
-),
+    WITH bikeumeter_activities AS (
+        SELECT date, start_city, end_city, public_transport_fare, ride_duration_minutes
+        FROM read_csv_auto('data/bikeumeter_commute_activities.csv') AS activities
+    ),
 
-cleaned_fare_euros AS (
-    SELECT month_number, month, ride_duration_minutes, 
-            REPLACE(public_transport_fare, '€', '') AS cleaned_fare_euros
-    FROM monthly_activities
-),
+    -- 1) Date manipulation
+        -- a) Keep only date portion of timestamp
+    clean_date AS (
+        SELECT *,
+                CAST(date AS DATE) AS date_only
+        FROM bikeumeter_activities
+    ),
 
-monthly_rides AS (
-    SELECT month_number, month,
-            COUNT(*) AS rides_per_month,
-            SUM(ride_duration_minutes) AS time_ridden,
-            SUM(CAST(cleaned_fare_euros AS INT)) AS saved_money_euros
-    FROM cleaned_fare_euros
-    GROUP BY month_number, month
-    ORDER BY month_number ASC
-),
+    -- b) Turn date into month -- monthname(date) function
+    -- c) Group activities by date
+    monthly_activities AS (
+        SELECT date_part('month', date_only) AS month_number, MONTHNAME(date_only) AS month, public_transport_fare, ride_duration_minutes,
+        FROM clean_date
+        WHERE date_part('year', date_only) = 2025
+        ORDER BY date_only -- always order by date not by month name
+    ),
 
-clean_monthly_rides AS (
-    SELECT month_number, month, rides_per_month,
-            CAST((time_ridden / 60) AS DECIMAL(10, 2)) AS hours_ridden,
-            saved_money_euros
-    FROM monthly_rides
-)
+    cleaned_fare_euros AS (
+        SELECT month_number, month, ride_duration_minutes, 
+                REPLACE(public_transport_fare, '€', '') AS cleaned_fare_euros
+        FROM monthly_activities
+    ),
+    -- 2) Public transport fare sum
+        -- a) Cummulative sum of fare per month
+    monthly_rides AS (
+        SELECT month_number, month,
+                COUNT(*) AS rides_per_month,
+                SUM(ride_duration_minutes) AS time_ridden,
+                SUM(CAST(cleaned_fare_euros AS INT)) AS saved_money_euros
+        FROM cleaned_fare_euros
+        GROUP BY month_number, month
+        ORDER BY month_number ASC
+    ),
 
-SELECT * FROM clean_monthly_rides;
--- 2) Public transport fare sum
-    -- a) Cummulative sum of fare per month
-    -- b) Get average from this
-    -- c) Calculate average from the latest 6 months
+    clean_monthly_rides AS (
+        SELECT month_number, month, rides_per_month,
+                CAST((time_ridden / 60) AS DECIMAL(10, 2)) AS hours_ridden,
+                saved_money_euros
+        FROM monthly_rides
+    )
+
+    SELECT * FROM clean_monthly_rides;
