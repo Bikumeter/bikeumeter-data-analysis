cbcc65a8eab9427900bb2639c8dbac0be8643615
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-01-20 15:25:15 +0100

    chore: adding first data cleaning sql code

--- sql/monthly_bike_savings.sql
+++ sql/monthly_bike_savings.sql
@@ -1,8 +1,28 @@
--- 1) Date manipulation
-    -- a) Turn date into month
-    -- b) Group activities by date
-    -- c) Get the last 6 months
+WITH bikeumeter_activities AS (
+    SELECT date, start_city, end_city, public_transport_fare, ride_duration_minutes
+    FROM read_csv_auto('data/bikeumeter_commute_activities.csv') AS activities
+),
 
+-- 1) Date manipulation
+    -- a) Keep only date portion of timestamp
+clean_date AS (
+    SELECT *,
+            CAST(date AS DATE) AS date_only
+    FROM bikeumeter_activities
+),
+
+-- b) Turn date into month -- monthname(date) function
+-- c) Group activities by date
+-- d) Get the last 6 months
+monthly_activities AS (
+    SELECT start_city, end_city, public_transport_fare, ride_duration_minutes,
+            MONTHNAME(date_only) AS month
+    FROM clean_date
+    ORDER BY date_only -- always order by date not by month name
+)
+
+
+SELECT * FROM monthly_activities;
 -- 2) Public transport fare sum
     -- a) Cummulative sum of fare per month
     -- b) Get average from this
