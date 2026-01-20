a490bb76adca0e1181146dfb8f2fb9046f9ebe8f
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-01-20 14:45:10 +0100

    chore: adding the tasks to structure the query writing process

---
+++ sql/monthly_bike_savings.sql
@@ -0,0 +1,9 @@
+-- 1) Date manipulation
+    -- a) Turn date into month
+    -- b) Group activities by date
+    -- c) Get the last 6 months
+
+-- 2) Public transport fare sum
+    -- a) Cummulative sum of fare per month
+    -- b) Get average from this
+    -- c) Calculate average from the latest 6 months
