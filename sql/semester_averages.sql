c89bcfd0aa2a52260ea39d35955dc5e36543b253
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-02-12 17:02:29 +0100

    chore: added the calculation for the price per km

--- sql/semester_averages.sql
+++ sql/semester_averages.sql
@@ -47,12 +47,20 @@ COPY (
                     CAST((AVG(mts_per_month) / 1000.0) AS DECIMAL(10,2)) AS semester_avg_kms,
                     CAST(ROUND(AVG(saved_money_euros), 2) AS DECIMAL(10,2)) AS semester_avg_saved_money
             FROM monthly_rides
-        )
+        ),
 
-        SELECT semester_avg_rides, semester_avg_hours_ridden, semester_avg_kms,
+        semester_average_calculation AS (
+            SELECT semester_avg_rides, semester_avg_hours_ridden, semester_avg_kms,
                 CAST(semester_avg_kms / semester_avg_rides AS DECIMAL(10,2)) AS semester_avg_ride_dt_km,
                 semester_avg_saved_money
-        FROM semester_averages
+            FROM semester_averages
+        )
+
+
+        SELECT semester_avg_rides, semester_avg_hours_ridden, semester_avg_kms,
+                CAST(semester_avg_saved_money / semester_avg_kms AS DECIMAL(10,2)) AS semester_avg_price_per_km,
+                semester_avg_saved_money
+        FROM semester_averages;
 )
 TO 'results/bike_semester_analysis.csv'
 WITH (HEADER, DELIMETER ',');
